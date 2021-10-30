e2e34fe314f79de4ab218d397f6f192ef47af9bf
Hassan Akolade <akoladeakinwande@hotmail.com>
2021-10-30 17:45:12 +0100

    setting up front page, js and css files linked

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+var getButton = document.querySelector(".navbar-toggler");
+
+
+  
+    var showText = function (target, message, index, interval) {   
+      if (index < message.length) {
+        $(target).append(message[index++]);
+     
+        setTimeout(function () { showText(target, message, index, interval); }, interval);
+       
+      }
+    }
+    $(function () {
+      showText("#VictoryHouseText", " Front End Developer", 0, 150); 
+    
+      
+    });
+    
+    
